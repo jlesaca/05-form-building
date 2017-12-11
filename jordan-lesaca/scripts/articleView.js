e4744f28c2fdbd7fd8800d7f3adc750dc6b5401d
Jordan <JordanLesaca@Gmail.Com>
2017-12-10 18:35:58 -0800

    added an event handler to update the preview and the export field

--- jordan-lesaca/scripts/articleView.js
+++ jordan-lesaca/scripts/articleView.js
@@ -76,12 +76,12 @@ articleView.setTeasers = () => {
 // COMMENT: Where is this function called? Why?
 // PUT YOUR RESPONSE HERE
 articleView.initNewArticlePage = () => {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-
+  // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  $('.tab-content').show()
 
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
-
+  $('#article-export').hide
   $('#article-json').on('focus', function(){
     this.select();
   });
@@ -111,6 +111,7 @@ articleView.create = () => {
 // COMMENT: Where is this function called? Why?
 // PUT YOUR RESPONSE HERE
 articleView.initIndexPage = () => {
+  articles.forEach(article => $('#artciles').append(article.toHtml()));
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
